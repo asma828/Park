11540691bbcb4b557ad23d51344e38f7c2a6c6c4
asma828 <asmalachhab66@gmail.com>
2025-03-13 15:37:43 +0000

    add admin and user routes

--- Park/routes/api.php
+++ Park/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ParkingController;
 use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\AdminController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -27,8 +29,18 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'role:user'])->group(function () {
     Route::get('/parkings', [ParkingController::class, 'index']);
     Route::post('/reservations', [ReservationController::class, 'store']);
     Route::get('/reservations', [ReservationController::class, 'index']);
+    Route::get('/parkings/search', [ParkingController::class, 'search']);
+    Route::put('/reservations/{id}', [ReservationController::class, 'update']);
+    Route::delete('/reservations/{id}', [ReservationController::class, 'cancel']);
+    Route::get('/reservations/history', [ReservationController::class, 'history']);
+});
+
+Route::middleware(['auth:sanctum', 'role:admin'])->prefix('admin')->group(function () {
+    Route::resource('parkings', AdminController::class);
+    // Route::post('parkings', [AdminController::class, 'store']);
+    Route::get('statistics', [AdminController::class, 'statistics']);
 });
